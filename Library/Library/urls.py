cedf3cf5b812d14eedba1b61549835c1063823d9
Govind-Nagar18 <nagargovind2004@gmail.com>
2025-05-16 17:32:36 +0530

    Add missing urls.py for project

--- Library/Library/urls.py
+++ Library/Library/urls.py
@@ -4,8 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('api/', include('books.urls')),
-    path('auth/', include('custom_auth.urls')),
+    path('api/', include('Library.books.urls')),
+    path('auth/', include('Library.custom_auth.urls')),
     path('admin/', admin.site.urls)
 ]
 
