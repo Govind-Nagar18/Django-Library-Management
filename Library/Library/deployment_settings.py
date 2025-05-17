a8d1324addd444bc2739e0a724d40bdc80f70e87
Govind-Nagar18 <nagargovind2004@gmail.com>
2025-05-17 16:31:08 +0530

    deploymentsetting file update

--- Library/Library/deployment_settings.py
+++ Library/Library/deployment_settings.py
@@ -4,7 +4,7 @@ from .settings import *
 from .settings import BASE_DIR
 
 ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
-CORS_ALLOW_ALL_ORIGINS = ['https://'+os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
+CORS_ALLOWED_ORIGINS = ['https://'+os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
 DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
